38100fdc70fd58c021de681627af6df347346f31
Trey <tbeau5595@hotmail.com>
2018-08-23 20:12:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function subtract(a, b) {
   number = a - b;
   return number;
 }
+function multiply(a, b) {
+  number = a * b;
+  return number;
+}
+function divide(a, b) {
+  number = a / b;
+  return number;
+}
